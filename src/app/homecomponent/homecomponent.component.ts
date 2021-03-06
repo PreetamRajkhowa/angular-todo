a7dbb9ab7056cb142f514b5ebc0daaff59ffbdde
Preetam Rajkhowa <preetam.rajkhowa.03@gmail.com>
2021-03-06 16:49:32 +0530

    WIP

--- src/app/homecomponent/homecomponent.component.ts
+++ src/app/homecomponent/homecomponent.component.ts
@@ -1,6 +1,13 @@
-import { TodoserviceService } from './../core/services/todoservice.service';
-import { Component, OnInit } from '@angular/core';
-import { Todo } from './../core/models/Todo.model';
+import {
+  TodoserviceService
+} from './../core/services/todoservice.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Todo
+} from './../core/models/Todo.model';
 
 
 
@@ -13,30 +20,36 @@ export class HomecomponentComponent implements OnInit {
 
   todolist: Todo;
   userid: number;
-  title:string;
-  body:string;
-  successMessage:string;
+  title: string;
+  body: string;
+  successMessage: string;
+  formShow: boolean = true;
 
   constructor(private todoService: TodoserviceService) {
-    this.todolist=new Todo(this.userid,this.title,this.body);
+    this.todolist = new Todo(this.userid, this.title, this.body);
   }
 
   ngOnInit(): void {
 
   }
 
-  onTodoFormSubmit(formdata){
+  onTodoFormSubmit(formdata: Todo) {
     this.todoService.submitToDoList(formdata).subscribe(
-      (data)=>{
-        this.successMessage='Data Saved Successfully!!';
-        this.resetData();
+      (data) => {
+        this.successMessage = 'Data Saved Successfully!!';
+        this.formShow = false;
       }
     );
 
   };
 
-  resetData(){
-    this.todolist=new Todo(this.userid,this.title,this.body);
+  resetData() {
+    this.todolist = new Todo(this.userid, this.title, this.body);
+  };
+
+  createAnotherTodo() {
+    this.resetData();
+    this.formShow = true;
   };
 
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,10 @@
-import { TodoserviceService } from './../core/services/todoservice.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  TodoserviceService
+} from './../core/services/todoservice.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 
 @Component({
@@ -9,22 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
- todoList:any;
- isLoading:boolean;
+  todoList: any;
+  isLoading: boolean;
 
   constructor(private Todoservice: TodoserviceService) {
     this.getToDoList();
-   }
-
-  ngOnInit(): void {
-    this.isLoading=true;
   }
 
-  getToDoList(){
+  ngOnInit(): void {
+    this.isLoading = true;
+  }
+
+  getToDoList() {
     this.Todoservice.getToDolist().subscribe(
-      (data)=>{
-        this.isLoading=false;
-        this.todoList=data;
+      (data) => {
+        this.isLoading = false;
+        this.todoList = data;
       }
     );
 
